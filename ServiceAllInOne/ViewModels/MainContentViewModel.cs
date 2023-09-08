fd6257c18d9bfe756b0c69dbc86a4c26549ecc2d
zhoujh <haoji_zhou@163.com>
2023-09-08 17:23:25 +0800

    ,,,,

---
+++ ServiceAllInOne/ViewModels/MainContentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAllInOne.ViewModels
+{
+    public sealed class MainContentViewModel : Screen
+    {
+        public ObservableCollection<ServicePanelViewModel> ServicePanels { get; private set; }
+
+        public MainContentViewModel()
+        {
+            ServicePanels = new ObservableCollection<ServicePanelViewModel>();
+#if DEBUG
+            for (int i = 0; i < 20; i++)
+            {
+                ServicePanels.Add(new ServicePanelViewModel
+                {
+                    Name = "Visual Studio Code + " + i,
+                });
+            }
+#endif
+        }
+    }
+}
---
+++ ServiceAllInOne/ViewModels/ServicePanelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAllInOne.ViewModels
+{
+    public sealed class ServicePanelViewModel : Screen
+    {
+        private string _name;
+
+        /// <summary>
+        /// 服务名称
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+    }
+}
